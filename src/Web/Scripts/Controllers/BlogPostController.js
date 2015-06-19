fe02c6b5f2f4a8443a1c6b3319abe77ca3e71155
Arthur Correa <acorrea@vistaprint.com>
2015-06-19 10:22:34 -0400

    moving blog save to use api

---
+++ src/Web/Scripts/Controllers/BlogPostController.js
@@ -0,0 +1,15 @@
+﻿function BlogPostController($scope, $resource, $http) {
+    $scope.getBlogPost = function (blogSubFolder, blogPostId) {
+        var getBlogPostRequest = $resource('/api/Blog/:blogSubFolder/BlogPost/:blogPostId');
+        $scope.blogPost = getBlogPost.get({ blogSubFolder: blogSubFolder, blogPostId: blogPostId });
+    }
+
+    $scope.saveBlogPost = function (blogSubFolder) {
+        var blogPostId = jQuery("#blogPostId").val();
+
+        $http.put('/api/Blog/' + blogSubFolder + '/BlogPost/' + blogPostId, $scope.blogPostForm)
+            .success(function (data) {
+                jQuery("#blogPostId").val(data.Id);
+            });
+    }
+}
